255c63d035b18afc5d013b91159dc04886e631ff
miriambeecham <miriam.beecham@gmail.com>
2025-10-02 20:37:00 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,10 @@ import TestimonialsPage from './pages/TestimonialsPage';
 import ContactPage from './pages/ContactPage';
 import AboutPage from './pages/AboutPage';
 import FAQPage from './pages/FAQPage';
-import CityWalnutCreekStaticPage from './pages/CityWalnutCreekStaticPage';
 import PrivateClassStaticPage from './pages/PrivateClassStaticPage';
-import PrivateClassDesiredEffectStaticPage from './pages/PrivateClassDesiredEffectStaticPage';
-import PublicClassMountDiabloYogaStaticPage from './pages/PublicClassMountDiabloYogaStaticPage';
-import PrivateClassWSECStaticPage from './pages/PrivateClassWSECStaticPage';
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
 import ScrollToTop from './components/ScrollToTop';
-import ClassPrepList from './pages/ClassPrepList';
-import BookingPage from './pages/BookingPage';
 import StripeSuccessPage from './pages/StripeSuccessPage';
-import StripeError from './pages/StripeErrorPage';
 import MotherDaughterBookingPage from './pages/MotherDaughterBookingPage';
 import AdultBookingPage from './pages/AdultBookingPage';
 import AdminClassPrepLinksPage from './pages/AdminClassPrepLinksPage';
@@ -45,23 +38,18 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/faq" element={<FAQPage />} />
             <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-            <Route path="/class-prep-list" element={<ClassPrepList />} />
             <Route path="/book-class/:classId" element={<BookingPage />} />
        
             <Route path="/book-adult-class" element={AdultBookingPage} />
             <Route path="/book-mother-daughter-class/:id" element={<MotherDaughterBookingPage />} />
             <Route path="/book-adult-class/:id" element={<AdultBookingPage />} />
-            
+       
           
 
-           // {/* Class Preparation Pages */}
-            <Route path="/public-cwc-prep" element={<CityWalnutCreekStaticPage />} />
+ 
+
             <Route path="/private-class-prep" element={<PrivateClassStaticPage />} />
-            <Route path="/public-class-desired-effect" element={<PrivateClassDesiredEffectStaticPage />} />
-            <Route path="/private-class-wsec-prep" element={<PrivateClassWSECStaticPage />} />
-            <Route path="/public-class-mdyoga-prep" element={<PublicClassMountDiabloYogaStaticPage />} />
             <Route path="/stripe-success" element={<StripeSuccessPage />} />
-            <Route path="/stripe-error" element={<StripeError />} />
             <Route path="/admin/class-prep-links" element={<AdminClassPrepLinksPage />} />
             <Route path="/class-prep/:scheduleId" element={<ClassPrepPage />} />
             
