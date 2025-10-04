577ed1ec013211dd85eca7b0006d3de0af6715d9
miriambeecham <miriam.beecham@gmail.com>
2025-10-04 15:02:43 -0700

    Update AdultBookingPage.tsx

--- src/pages/AdultBookingPage.tsx
+++ src/pages/AdultBookingPage.tsx
@@ -420,7 +420,10 @@ const AdultBookingPage: React.FC = () => {
                   <div className="flex items-center gap-3">
                     <Clock className="w-5 h-5 text-gray-500" />
                     <span className="text-gray-700">
-                      {classSchedule.start_time} - {classSchedule.end_time}
+                    {classSchedule.start_time_new ? 
+          `${new Date(classSchedule.start_time_new).toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true })} - ${new Date(classSchedule.end_time_new).toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true })}` :
+          `${classSchedule.start_time} - ${classSchedule.end_time}`
+        }
                     </span>
                   </div>
                   {classSchedule.location && (
