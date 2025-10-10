c8ec991dd47ca166d45a7f4f482a627abad312a7
miriambeecham <miriam.beecham@gmail.com>
2025-10-10 10:31:36 -0700

    Update ClassCard.tsx

--- src/components/ClassCard.tsx
+++ src/components/ClassCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Clock, MapPin } from 'lucide-react';
 import { format } from 'date-fns';
-import type { ClassSchedule } from '../lib/supabase';
+import type { ClassSchedule } from '../lib/airtable';
 
 interface ClassCardProps {
   classData: ClassSchedule & { class?: any };
