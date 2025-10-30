334ec53edf43f3411733f4c555e39578c7631089
miriambeecham <miriam.beecham@gmail.com>
2025-10-29 19:18:58 -0700

    Update verify-payment.js

--- api/verify-payment.js
+++ api/verify-payment.js
@@ -249,7 +249,6 @@ try {
     const { data, error } = await resend.emails.send({
       from: FROM_EMAIL,
       to: booking.fields['Contact Email'],
-      cc: 'confirmations@streetwiseselfdefense.com', 
       subject: 'Your Self Defense Class Registration is Confirmed!',
       html: emailHTML,
       attachments: [{ filename: 'class-event.ics', content: cal.toString() }]
